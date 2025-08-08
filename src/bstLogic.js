f9f65eea8873ccc5c1bfc24e95dad7f39864808f
Ulysse <ulysse.aubanton@gmail.com>
2025-08-08 17:56:53 +0200

    Build the buildTree func

--- src/bstLogic.js
+++ src/bstLogic.js
@@ -6,9 +6,48 @@ class Node {
     };
 };
 
-class Tree {
+export class Tree {
     constructor (array) {
         this.root = buildTree(array);
     };
 };
 
+function sortedArrayToBSTRecur(arr, start, end) {
+    if (start > end) return null;
+    
+    // Find the middle element
+    let mid = start + Math.floor((end - start) / 2);
+    
+    // Create root node
+    let root = new Node(arr[mid]);
+    
+    // Create left subtree
+    root.left = sortedArrayToBSTRecur(arr, start, mid - 1);
+    
+    // Create right subtree
+    root.right = sortedArrayToBSTRecur(arr, mid + 1, end);
+    
+    return root;
+};
+
+function buildTree (array) {
+    const sortedArray = array.toSorted((a,b)=> a-b);
+    const checkedArray = sortedArray.filter(function(item, pos, ary) {
+        return !pos || item != ary[pos - 1];
+    });
+    const root = sortedArrayToBSTRecur(checkedArray, 0, checkedArray.length-1);
+    return root;
+};
+
+export const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Tree , prettyPrint } from './bstLogic.js';
+
+const test = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 56, 298];
+const tree = new Tree (test);
+prettyPrint(tree.root);
